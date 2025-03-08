a620cce55bf78e80c901e08df398236b85c75510
Adit <adityashekhar207@gmail.com>
2025-03-08 19:42:25 +0530

    Initial Commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
+    return(
+        <footer className="footer">
+            <p>Copyright ©  {currentYear}</p>
+        </footer>
+    )
+}
+
+export default Footer;
